06fef973ac45d1edcceee7d3d57d9ce5b3c9526c
bosqueee <141823822+bosqueee@users.noreply.github.com>
2023-09-07 14:14:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-exports.textpro = require("./src/maker")
-exports.photooxy = require("./src/maker")
-exports.ephoto = require("./src/maker")
 exports.tiktok = require("./src/downloader").tiktok
-exports.instagram = require("./src/downloader").instagram
-exports.facebook = require("./src/downloader").facebook
